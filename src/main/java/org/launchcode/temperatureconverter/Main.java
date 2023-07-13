23c6b63ca552ae646e7eca9b7bfc20d8ccea48c5
Josephina Lacy <josephinalacy@gmail.com>
2023-07-12 19:40:11 -0500

    init

---
+++ src/main/java/org/launchcode/temperatureconverter/Main.java
@@ -0,0 +1,13 @@
+//package org.launchcode.temperatureconverter;
+//
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Configuration;
+//
+//@SpringBootApplication
+//@Configuration
+//public class Main {
+//    public static void main(String[] args){
+//        SpringBootApplication.run(Main.class);
+//    }
+//
+//}
